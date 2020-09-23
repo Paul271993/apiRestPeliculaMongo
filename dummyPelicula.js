8432012a07a3c37ea2cab86c15431ca49c59b77c
Paul271993 <paul.muresan1993@gmail.com>
2020-09-23 02:11:07 +0200

    first commit

---
+++ dummyPelicula.js
@@ -0,0 +1,69 @@
+//poblar base de datos
+require("./connection"); //coneccion a base de datos 
+
+const Pelicula = require("./pelicula"); // requerimos el modelo 
+
+let pelicula = new Pelicula({
+    title: "Rambo",
+    releaseYear: 1998,
+    nacionality: "EUA",
+    genre: "Action",
+    actors: ["PEPE", "Rambo", "JULIETA"],
+    directors: ["PEPE5", "Rambo5", "JULIETA5"],
+    writers: ["PEPE7", "Rambo7", "JULIETA7"],
+    plataform: "YOU TUBE",
+    isMCU: "NINGUNO",
+    mainCharacterName: "Rambo", //nombre del personaje principal
+    producer: "LALITO",
+    distributor: "MIGUEL"
+});
+
+pelicula.save((err, document) => {
+    if (err) {
+        console.log("Error al subir pelicula a la base de datos." + err);
+    }
+    console.log(" PELICULA GUARDADA CORRECTAMENTE." + document);
+});
+
+pelicula = new Pelicula({
+    title: "Pinocho",
+    releaseYear: 1992,
+    nacionality: "EUA",
+    genre: "Infantil",
+    actors: ["jepeto", "lol", "JULIETA"],
+    directors: ["PEPE5", "Rambo5", "JULIETA5"],
+    writers: ["PEPE7", "Rambo7", "JULIETA7"],
+    plataform: "YOU TUBE",
+    isMCU: "NINGUNO",
+    mainCharacterName: "Pinocho", //nombre del personaje principal
+    distributor: "MIGUEL"
+});
+
+pelicula.save((err, document) => {
+    if (err) {
+        console.log("Error al subir pelicula a la base de datos." + err);
+    }
+    console.log(" PELICULA GUARDADA CORRECTAMENTE." + document);
+})
+
+pelicula = new Pelicula({
+    title: "Quiricu",
+    releaseYear: 2000,
+    nacionality: "Africa",
+    genre: "Infantil",
+    actors: ["PEPE", "Rambo", "JULIETA"],
+    directors: ["PEPE5", "Rambo5", "JULIETA5"],
+    writers: ["PEPE7", "Rambo7", "JULIETA7"],
+    plataform: "Netflix",
+    isMCU: "NINGUNO",
+    mainCharacterName: "Rambo",
+    producer: "Javier",
+    distributor: "Samuekl"
+});
+
+pelicula.save((err, document) => {
+    if (err) {
+        console.log("Error al subir pelicula a la base de datos." + err);
+    }
+    console.log(" PELICULA GUARDADA CORRECTAMENTE." + document);
+});
